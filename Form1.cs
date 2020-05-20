cbdd5e1a1defa6f827a5431095c485822eef12f6
Arvils <arvils.apsitis@gmail.com>
2020-05-20 18:20:24 +0300

    final ver 2020.05.20.

--- Form1.cs
+++ Form1.cs
@@ -12,11 +12,44 @@ namespace TickTackToe
 {
     public partial class Form1 : Form
     {
+        
+        bool xPlayerTurn = true;
         public Form1()
         {
             InitializeComponent();
+            InitializeCells();
+            InitializeGrid();
         }
 
-        SetupGame()
+        private void InitializeCells() 
+        {
+            this.BackColor = Color.LightGray;
+            string labelName;
+            for (int c = 1; c <= 9; c++) //LOL
+            {
+                labelName = "label" + c;
+                Grid.Controls[labelName].Text = string.Empty;
+            }
+        }
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightGray;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble;
+        }
+
+        private void Player_click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            if (xPlayerTurn)
+            {
+                label.Text = "X";
+            }
+            else 
+            {
+                label.Text = "O";
+            }
+            xPlayerTurn = !xPlayerTurn;
+        }
+
     }
 }
